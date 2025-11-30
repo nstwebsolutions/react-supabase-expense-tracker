874de91fd8671b36c9d71dd0de018c1db5012e26
sarfarazkasmani <yusufkasmani491@gmail.com>
2025-11-30 21:00:40 +0530

    UI done for registration and login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,53 @@
+import { Form, Input, Button } from "antd";
+import { Link } from "react-router-dom";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+  return (
+    <div className='bg-gray-200 h-screen flex justify-center items-center'>
+      <div className='bg-white border border-gray-300 p-5 rounded w-[420px] shadow-sm'>
+        <h1 className='text-xl font-bold'>Login</h1>
+        <p className='text-sm font-semibold'>
+          Welcome back! Please login to your account.
+        </p>
+        <hr className='border-gray-300 my-5' />
+        <Form layout='vertical' autoComplete='off' onFinish={onFinish}>
+          <Form.Item
+            label='Email'
+            name='email'
+            rules={[
+              {
+                required: true,
+                message: "Please enter your email",
+              },
+            ]}
+          >
+            <Input placeholder='Email' />
+          </Form.Item>
+          <Form.Item
+            label='Password'
+            name='password'
+            rules={[
+              {
+                required: true,
+                message: "Please enter your password",
+              },
+            ]}
+          >
+            <Input.Password placeholder='Password' />
+          </Form.Item>
+          <Button className='mb-3' block type='primary' htmlType='submit'>
+            LOGIN
+          </Button>
+          <span className='text-sm font-semibold'>
+            Don't have an account? <Link to='/register'>Register</Link>
+          </span>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,5 +1,65 @@
+import { Form, Input, Button } from "antd";
+import { Link } from "react-router-dom";
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+  return (
+    <div className='bg-gray-200 h-screen flex justify-center items-center'>
+      <div className='bg-white border border-gray-300 p-5 rounded w-[420px] shadow-sm'>
+        <h1 className='text-xl font-bold'>Register</h1>
+        <p className='text-sm font-semibold'>
+          Create an account to get started with our services.
+        </p>
+        <hr className='border-gray-300 my-5' />
+        <Form layout='vertical' autoComplete='off' onFinish={onFinish}>
+          <Form.Item
+            label='Name'
+            name='name'
+            rules={[
+              {
+                required: true,
+                message: "Please enter your full name",
+              },
+            ]}
+          >
+            <Input placeholder='Name' />
+          </Form.Item>
+          <Form.Item
+            label='Email'
+            name='email'
+            rules={[
+              {
+                required: true,
+                message: "Please enter your email",
+              },
+            ]}
+          >
+            <Input placeholder='Email' />
+          </Form.Item>
+          <Form.Item
+            label='Password'
+            name='password'
+            rules={[
+              {
+                required: true,
+                message: "Please enter your password",
+              },
+            ]}
+          >
+            <Input.Password placeholder='Password' />
+          </Form.Item>
+          <Button className='mb-3' block type='primary' htmlType='submit'>
+            REGISTER
+          </Button>
+          <span className='text-sm font-semibold'>
+            Already have an account? <Link to='/login'>Login</Link>
+          </span>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
